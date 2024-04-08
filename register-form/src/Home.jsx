fc0674620957bc6601828aeaa97eac77307078cb
BellTitiporn <139092027+BellTitiporn@users.noreply.github.com>
2024-04-08 15:34:35 +0700

    Update Home.jsx

--- register-form/src/Home.jsx
+++ register-form/src/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import LockIcon from "@mui/icons-material/Lock";
+import "./Home.css";
 
 function Home() {
-  return <div>AA</div>;
+  return (
+    <div>
+      <div className="lock-icon">
+        <LockIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
